bdfe0b14e6cd5b161ae91a87c92aae684f513386
derstolz <a>
2021-02-13 01:46:55 +0100

    passing network dump files to the netstat map

--- map.py
+++ map.py
@@ -5,6 +5,42 @@ from netstat import netstat
 
 app = Flask(__name__)
 
+DEFAULT_IP = '127.0.0.1'
+DEFAULT_PORT = 5000
+
+
+def get_arguments():
+    from argparse import ArgumentParser
+    parser = ArgumentParser()
+    parser.add_argument('--ip',
+                        dest='ip',
+                        required=False,
+                        default=DEFAULT_IP,
+                        type=str,
+                        help='Specify the IP address to bind to. '
+                             f'Default is {DEFAULT_IP}')
+    parser.add_argument('--port',
+                        dest='port',
+                        required=False,
+                        default=DEFAULT_PORT,
+                        type=str,
+                        help='Specify the TCP port to bind to. '
+                             f'Default is {DEFAULT_PORT}')
+    parser.add_argument('--file',
+                        dest='file',
+                        required=False,
+                        type=str,
+                        help='Provide a txt file with execution result of the netstat command '
+                             '(Unix: "netstat -tupn", Windows: "netstat -ano") '
+                             'to build the map with network connections. '
+                             'By default the script attempts to retrieve network connections '
+                             'of its current host machine.')
+    options = parser.parse_args()
+    return options
+
+
+options = get_arguments()
+
 
 @app.route('/')
 def index():
@@ -24,10 +60,11 @@ def map():
 def update():
     client_procs = request.get_json(force=True)
     app.logger.info(f"Client has layers: {client_procs}")
-    new_markers, new_procs = netstat.run(known_procs=client_procs)
+    new_markers, new_procs = netstat.run(known_procs=client_procs,
+                                         netstat_file=options.file)
     app.logger.info(f"Sending layers: {new_procs}")
     return jsonify([new_markers, new_procs])
 
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host=options.ip, port=options.port, debug=True)
